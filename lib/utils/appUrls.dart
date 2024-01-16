4ac58fb8fc7dbcb953477cf198a775e22f4596e2
Hritik Bhardwaj <hritikbhardwaj41@gmail.com>
2024-01-16 17:51:07 +0530

    fw
    wf

--- lib/utils/appUrls.dart
+++ lib/utils/appUrls.dart
@@ -3,7 +3,8 @@ class AppUrl {
   //https://alpha-ecom.developmentalphawizz.com
   //https://test-alpha-ecom.developmentalphawizz.com
 
-  static const baseURL = "https://alpha-ecom.developmentalphawizz.com/api/v1/";
+  static const baseURL =
+      "https://test-alpha-ecom.developmentalphawizz.com/api/v1/";
   static String sendLoginOtp = "${baseURL}auth/send-login-otp";
   static String socialLogin = "${baseURL}auth/social-login";
   static String sendRegisterOtp = "${baseURL}auth/send-otp";
